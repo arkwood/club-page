27483edc928d79ae5dde3f020a579108a85cb1d3
= <peterilf@au1.ibm.com>
2015-02-18 19:57:24 +1100

    Added comment and forced re-commit;

--- admin/setup/setup.php
+++ admin/setup/setup.php
@@ -8,6 +8,10 @@ mysql_connect(MYSQL_HOST, MYSQL_USER, MYSQL_PASSWORD);
 mysql_select_db(MYSQL_DATABASE);
 
 foreach (scandir('datamodel') as $entry) {
+    // skip current directory and parent directory
+    if ($entry === "." || $entry === "..")
+                continue;
+    
     require_once 'datamodel/' . $entry; 
 }
 ?>
