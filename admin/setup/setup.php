a79299ff52b14da1c2aaf1f501ae1489dd53d080
= <peterilf@au1.ibm.com>
2015-02-18 19:54:17 +1100

    Fixed syntax error in setup and added error output;

--- admin/setup/setup.php
+++ admin/setup/setup.php
@@ -1,5 +1,8 @@
 <?php
-require_once('../../config.php');
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+require_once '../../config.php';
 
 mysql_connect(MYSQL_HOST, MYSQL_USER, MYSQL_PASSWORD);
 mysql_select_db(MYSQL_DATABASE);
